a9fd510bf6e950f78136540685d53c79537875f5
264hf2jztj-ship-it <264hf2jztj@privaterelay.appleid.com>
2026-08-31 21:35:48 +0200

    Step 2: aggiunge configurazione Supabase

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+"use strict";
+
+export const SUPABASE_URL = "https://zwevsjvvuycbkqddwiky.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_lUO_utojfK-rDSve4o3EUQ_a2gPgmhm";
+
+export const LOGIN_ALIASES = Object.freeze({
+  PLAN_OFCN: "plan-ofcn@schede-operative-ofcn.invalid",
+});
